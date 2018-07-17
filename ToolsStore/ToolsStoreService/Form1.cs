1c7cd6d4fa925bf9938b726f51739adde28a1c3e
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-07-17 22:53:57 +0300

    program & form1 for test

---
+++ ToolsStore/ToolsStoreService/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolsStoreService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string conn;
+            conn = System.Configuration.ConfigurationManager.ConnectionStrings["ToolsStoreConnectionString"].ToString();
+            //Log.write(conn);
+            //FileManager.LoadFiles();
+        }
+    }
+}
--- ToolsStore/ToolsStoreService/Program.cs
+++ ToolsStore/ToolsStoreService/Program.cs
@@ -14,6 +14,10 @@ namespace ToolsStoreService
         /// </summary>
         static void Main()
         {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
