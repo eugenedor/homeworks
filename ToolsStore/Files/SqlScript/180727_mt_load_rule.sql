476658e41f944da7086305634473a92d0277236a
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-07-28 00:44:31 +0300

    get load rule

--- ToolsStore/Files/SqlScript/180727_mt_load_rule.sql
+++ ToolsStore/Files/SqlScript/180727_mt_load_rule.sql
@@ -38,14 +38,23 @@ CREATE TABLE dbo.MT_LOAD_RULE(
 	FileName    nvarchar(100) NOT NULL,
 	IsActive    bit NOT NULL,
 	MethodLoad  nvarchar(100) NOT NULL,
-	Descr       nvarchar(250) NOT NULL,	
-	Ord         int NOT NULL,
 	PathToXsd   nvarchar(250) NOT NULL,	
 	XsdName     nvarchar(100) NOT NULL,	
+	Descr       nvarchar(250) NOT NULL,	
+	Ord         int NOT NULL,
  CONSTRAINT PK_MT_LOAD_RULE PRIMARY KEY CLUSTERED 
 (
 	LoadRuleId ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+
+--
+--insert mt_load_rule
+--
+INSERT INTO dbo.MT_LOAD_RULE (Code, FileName, IsActive, MethodLoad, PathToXsd, XsdName, Descr, Ord)
+     VALUES ('Category', 'Category.xml', 1, 'LoadCategory', '\xsd\', 'Category.xsd', 'Категории', 1);
+INSERT INTO dbo.MT_LOAD_RULE (Code, FileName, IsActive, MethodLoad, PathToXsd, XsdName, Descr, Ord)
+     VALUES ('Vat', 'Vat.xml', 1, 'LoadVat', '\xsd\', 'Vat.xsd', 'НДС', 2);
 GO
