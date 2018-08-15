247a003e4e33253e2937a2373dd142f520245c36
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-08-15 23:57:58 +0300

    ToolsStore

---
+++ ToolsStore/ToolsStore.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToolsStore.WebUI.MvcApplication" Language="C#" %>
---
+++ ToolsStore/ToolsStore.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToolsStore.WebUI.Startup))]
+namespace ToolsStore.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
