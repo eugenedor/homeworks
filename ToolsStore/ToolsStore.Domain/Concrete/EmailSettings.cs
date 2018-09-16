e323ed033297295e9c6bb8d5b75fcf7da2849d51
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-09-16 23:58:08 +0300

    IOrderProcessor, EmailOrderProcessor

---
+++ ToolsStore/ToolsStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "toolsstore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true; //false;
+        public string FileLocation = @"c:\tools_store_emails";
+    }
+}
