96b9c0b6f044742044c141d8272211f5e67c4357
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-09-20 23:54:00 +0300

    create table cart

--- ToolsStore/Files/SqlVersion/180918_2324_create_shipping.sql
+++ ToolsStore/Files/SqlVersion/180918_2324_create_shipping.sql
@@ -15,3 +15,25 @@ CREATE TABLE [dbo].[RS_SHIPPING]
 	[GiftWrap] [bit] NOT NULL,
  CONSTRAINT [PK_RS_SHIPPING] PRIMARY KEY CLUSTERED ([ShippingId])
 ) ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[RS_CART]
+(
+	[CartId] [bigint] IDENTITY(1,1) NOT NULL,
+	[ShippingId] [bigint] NOT NULL,
+	[ProductId] [bigint] NOT NULL,
+	[PriceId] [bigint] NULL,
+	[Quantity] [int] NOT NULL
+CONSTRAINT [PK_RS_CART] PRIMARY KEY CLUSTERED ([CartId])
+) ON [PRIMARY]
+GO
+
+CREATE INDEX [FK_RS_CART_RS_SHIPPING]
+  ON [dbo].[RS_CART] ([ShippingId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[RS_CART]
+  ADD CONSTRAINT [FK_RS_CART_RS_SHIPPING] FOREIGN KEY ([ShippingId]) REFERENCES [dbo].[RS_SHIPPING] ([ShippingId])
+GO
