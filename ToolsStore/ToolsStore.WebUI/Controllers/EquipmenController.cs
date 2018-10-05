e98ba73f5a826d58285f674f66214b859c6c0eea
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-10-05 23:49:35 +0300

    equipments cshtml

---
+++ ToolsStore/ToolsStore.WebUI/Controllers/EquipmenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Infrastructure;
+using ToolsStore.Domain.Abstract;
+using ToolsStore.Domain.Entities;
+
+namespace ToolsStore.WebUI.Controllers
+{
+    public class EquipmenController : Controller
+    {
+        private IProductRepository repository;
+
+        public EquipmenController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Equipments()
+        {
+            return View(((DbQuery<SK_EQUIPMENT>)repository.Equipments).Include("CT_CATEGORY"));
+        }
+    }
+}
