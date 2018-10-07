9861cbe2e5a495d419a0206bf03a9d6a17e59fa2
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-10-07 11:37:21 +0300

    CRUD equipment

--- ToolsStore/ToolsStore.WebUI/Controllers/EquipmenController.cs
+++ ToolsStore/ToolsStore.WebUI/Controllers/EquipmenController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Data.Entity.Infrastructure;
 using ToolsStore.Domain.Abstract;
 using ToolsStore.Domain.Entities;
+using ToolsStore.WebUI.Models;
 
 namespace ToolsStore.WebUI.Controllers
 {
@@ -26,23 +27,54 @@ namespace ToolsStore.WebUI.Controllers
         public ViewResult EquipmentEdit(long equipmentId)
         {
             SK_EQUIPMENT equipment = repository.Equipments.Where(p => p.EquipmentId == equipmentId).FirstOrDefault();
-            return View(equipment);
+            IEnumerable<CT_CATEGORY> categories = repository.Categories;
+
+            EquipmentViewModel model = new EquipmentViewModel
+            {
+                Equipment = equipment,
+                Categories = categories
+            };
+
+            return View(model);
         }
 
         [HttpPost]
-        public ActionResult EquipmentEdit(SK_EQUIPMENT equipment)
+        public ActionResult EquipmentEdit(EquipmentViewModel model)
         {
             if (ModelState.IsValid)
             {
-                repository.SaveEquipment(equipment);
-                TempData["message"] = string.Format("{0} сохранено", equipment.Code);
+                repository.SaveEquipment(model.Equipment);
+                TempData["message"] = string.Format("{0} сохранено", model.Equipment.Code);
                 return RedirectToAction("Equipments");
             }
             else
             {
                 // что-то не так с значениями данных (there is something wrong with the data values)
-                return View(equipment);
+                return View(model);
             }
+        }
+
+        public ViewResult EquipmentCreate()
+        {
+            IEnumerable<CT_CATEGORY> categories = repository.Categories;
+            EquipmentViewModel model = new EquipmentViewModel
+            {
+                Equipment = new SK_EQUIPMENT(),
+                Categories = categories
+            };
+
+            return View("EquipmentEdit", model);
+        }
+
+        [HttpPost]
+        public ActionResult EquipmentDelete(long equipmentId)
+        {
+            SK_EQUIPMENT deletedEquipment = repository.DeleteEquipment(equipmentId);
+            if (deletedEquipment != null)
+            {
+                TempData["message"] = string.Format("{0} был удален", deletedEquipment.Code);
+            }
+            return RedirectToAction("Equipments");
         }
     }
 }
---
+++ ToolsStore/ToolsStore.WebUI/Models/EquipmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToolsStore.Domain.Entities;
+
+namespace ToolsStore.WebUI.Models
+{
+    public class EquipmentViewModel
+    {
+        public SK_EQUIPMENT Equipment { get; set; }
+
+        public IEnumerable<CT_CATEGORY> Categories { get; set; }
+    }
+}
