34d2274aee4888a8d69658c9d82cc91dd63c7b4d
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-10-15 23:17:32 +0300

    trigger tr_for_ins_upd_rs_price

---
+++ ToolsStore/Files/SqlScript/180921_0033_create_order.sql
@@ -0,0 +1,61 @@
+USE [ToolsStore]
+GO
+
+CREATE TABLE [dbo].[RS_ORDER]
+(
+	[OrderId] [bigint] IDENTITY(1,1) NOT NULL,
+	[Surname] [nvarchar](100) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Phone] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](50) NULL,
+	[Line1] [nvarchar](250) NOT NULL,
+	[Line2] [nvarchar](150) NULL,
+	[Line3] [nvarchar](150) NULL,
+	[City] [nvarchar](100) NOT NULL,
+	[State] [nvarchar](100) NOT NULL,
+	[Zip] [nvarchar](50) NULL,
+	[Country] [nvarchar](100) NOT NULL,
+	[GiftWrap] [bit] NOT NULL,
+	[DateOrder] [datetime] NULL,
+ CONSTRAINT [PK_RS_ORDER] PRIMARY KEY CLUSTERED ([OrderId])
+) ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[RS_CART]
+(
+	[CartId] [bigint] IDENTITY(1,1) NOT NULL,
+	[OrderId] [bigint] NOT NULL,
+	[ProductId] [bigint] NOT NULL,
+	[PriceId] [bigint] NULL,
+	[Quantity] [int] NOT NULL
+CONSTRAINT [PK_RS_CART] PRIMARY KEY CLUSTERED ([CartId])
+) ON [PRIMARY]
+GO
+
+CREATE INDEX [FK_RS_CART_RS_ORDER]
+  ON [dbo].[RS_CART] ([OrderId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[RS_CART]
+  ADD CONSTRAINT [FK_RS_CART_RS_ORDER] FOREIGN KEY ([OrderId]) REFERENCES [dbo].[RS_ORDER] ([OrderId])
+GO
+
+CREATE INDEX [FK_RS_CART_RS_PRODUCT]
+  ON [dbo].[RS_CART] ([ProductId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[RS_CART]
+  ADD CONSTRAINT [FK_RS_CART_RS_PRODUCT] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[RS_PRODUCT] ([ProductId])
+GO
+
+CREATE INDEX [FK_RS_CART_RS_PRICE]
+  ON [dbo].[RS_CART] ([PriceId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[RS_CART]
+  ADD CONSTRAINT [FK_RS_CART_RS_PRICE] FOREIGN KEY ([PriceId]) REFERENCES [dbo].[RS_PRICE] ([PriceId])
+GO
---
+++ ToolsStore/Files/SqlScript/181015_2313_drop_create_triggers.sql
@@ -0,0 +1,64 @@
+USE ToolsStore
+GO
+
+IF DB_NAME() <> N'ToolsStore' SET NOEXEC ON
+GO
+
+--
+-- Change transaction localization level (Изменить уровень локализации транзакции)
+--
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+
+--
+-- begin transaction
+--
+BEGIN TRANSACTION
+GO
+
+DROP TRIGGER dbo.TR_FOR_INS_UPD_RS_PRODUCT
+GO
+IF @@ERROR<>0 OR @@TRANCOUNT=0 BEGIN IF @@TRANCOUNT>0 ROLLBACK SET NOEXEC ON END
+GO
+
+CREATE TRIGGER dbo.TR_FOR_INS_UPD_RS_PRICE ON dbo.RS_PRICE FOR INSERT, UPDATE
+AS
+BEGIN TRAN
+  DECLARE @error NVARCHAR(100);
+  DECLARE @maxDate DATETIME = CAST('9999-12-31 23:59:59.997' AS DATETIME);
+
+  --dateend--
+  UPDATE dbo.RS_PRICE
+  SET DateEnd = DATEADD(dd, 1, DATEADD(ms, -3, DateEnd))
+  WHERE PriceId = (SELECT i.PriceId FROM INSERTED i);
+
+  --check--
+  IF (EXISTS(SELECT prc.PriceId
+             FROM INSERTED i
+			      JOIN RS_PRICE prc
+				    ON i.ProductId = prc.ProductId
+					   AND i.PriceId != prc.PriceId
+					   AND i.DateBegin <= ISNULL(prc.DateEnd, @maxDate)
+					   AND prc.DateBegin <= ISNULL(i.DateEnd, @maxDate)))
+  BEGIN
+	SET @error = 'Пересечение периодов цен.';
+    RAISERROR(@error, 16, 1)
+	ROLLBACK TRAN    
+  END
+  COMMIT TRAN
+GO
+IF @@ERROR<>0 OR @@TRANCOUNT=0 BEGIN IF @@TRANCOUNT>0 ROLLBACK SET NOEXEC ON END
+GO
+
+
+--
+-- commit transaction
+--
+IF @@TRANCOUNT>0 COMMIT TRANSACTION
+GO
+
+--
+-- set noexec to off (Установить NOEXEC в состояние off)
+--
+SET NOEXEC OFF
+GO
