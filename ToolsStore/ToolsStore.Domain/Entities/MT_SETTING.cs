71f080f139d8da97d1a6a2a266c05f86110ffd55
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-10-21 21:42:28 +0300

    settingcontroller and view settings

--- ToolsStore/ToolsStore.Domain/Entities/MT_SETTING.cs
+++ ToolsStore/ToolsStore.Domain/Entities/MT_SETTING.cs
@@ -1,28 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
 namespace ToolsStore.Domain.Entities
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
     public partial class MT_SETTING
     {
         [Key]
+        [HiddenInput(DisplayValue = false)]
         public long SettingId { get; set; }
 
-        [Required]
+        [Display(Name = "Код настройки")]
+        [Required(ErrorMessage = "Введите код настройки")]
         [StringLength(50)]
         public string Code { get; set; }
 
-        [Required]
+        [Display(Name = "Значение")]
+        [Required(ErrorMessage = "Введите значение")]
         [StringLength(250)]
         public string Value { get; set; }
 
-        [Required]
+        [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Введите описание")]
         [StringLength(250)]
+        [DataType(DataType.MultilineText)]
         public string Descr { get; set; }
 
+        [Display(Name = "Признак активности")]
+        [Required(ErrorMessage = "Укажите признак активности")]
         public bool IsActive { get; set; }
     }
 }
---
+++ ToolsStore/ToolsStore.WebUI/Controllers/SettingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToolsStore.Domain.Abstract;
+using ToolsStore.Domain.Entities;
+
+namespace ToolsStore.WebUI.Controllers
+{
+    public class SettingController : Controller
+    {
+        private ISettingRepository repository;
+
+        public SettingController(ISettingRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Settings()
+        {
+            return View(repository.Settings);
+        }
+    }
+}
