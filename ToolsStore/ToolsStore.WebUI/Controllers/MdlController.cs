b93d9b0d21f0fdfac935bdccf716ba03d32dd585
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-10-24 22:56:53 +0300

    mdlcontroller

---
+++ ToolsStore/ToolsStore.WebUI/Controllers/MdlController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Infrastructure;
+using ToolsStore.Domain.Abstract;
+using ToolsStore.Domain.Entities;
+using ToolsStore.WebUI.Models;
+
+namespace ToolsStore.WebUI.Controllers
+{
+    public class MdlController : Controller
+    {
+        private IProductRepository repository;
+
+        public MdlController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Models()
+        {
+            return View(((DbQuery<SK_MODEL>)repository.Models).Include("CT_BRAND"));
+        }
+    }
+}
---
+++ ToolsStore/ToolsStore.WebUI/Models/MdlViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToolsStore.Domain.Entities;
+
+namespace ToolsStore.WebUI.Models
+{
+    public class MdlViewModel
+    {
+        public SK_MODEL Mdl { get; set; }
+
+        public IEnumerable<CT_BRAND> Brand { get; set; }
+    }
+}
