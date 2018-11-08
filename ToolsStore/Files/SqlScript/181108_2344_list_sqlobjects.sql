3ff9d25825699d6d27a5a5d08b76e09b19f87e5a
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-11-08 23:52:24 +0300

    sql: list_sqlobjects

---
+++ ToolsStore/Files/SqlScript/181108_2344_list_sqlobjects.sql
@@ -0,0 +1,42 @@
+--
+--sysobjects + syscomments
+--
+
+--dor--	  
+SELECT DISTINCT o.id,
+                o.name AS ObjectName,
+                o.type, o.xtype,
+				CASE o.xtype
+					WHEN 'C' THEN 'CHECK constraint'
+					WHEN 'D' THEN 'Default or DEFAULT constraint'
+					WHEN 'F' THEN 'FOREIGN KEY constraint'
+					WHEN 'FN' THEN 'Scalar function'
+					WHEN 'IF' THEN 'In-lined table-function'
+					WHEN 'K' THEN 'PRIMARY KEY or UNIQUE constraint'
+					WHEN 'L' THEN 'Log'
+					WHEN 'P' THEN 'Stored procedure'
+					WHEN 'R' THEN 'Rule'
+					WHEN 'RF' THEN 'Replication filter stored procedure'
+					WHEN 'S' THEN 'System table'
+					WHEN 'TF' THEN 'Table function'
+					WHEN 'TR' THEN 'Trigger'
+					WHEN 'U' THEN 'User table'
+					WHEN 'V' THEN 'View'
+					WHEN 'X' THEN 'Extended stored procedure'
+					ELSE o.xtype
+				END AS ObjectType,
+				c.text
+FROM sysobjects o
+	 JOIN syscomments c ON o.id = c.id
+WHERE o.type in ('P', 'FN', 'TF', 'TR', 'V', 'U')
+	  AND lower(c.text) LIKE '%rem%'
+ORDER BY ObjectName;
+
+
+--kurd--
+SELECT o.object_id, o.name, o.type, o.type_desc, c.text	
+FROM sys.objects o JOIN
+	 syscomments c ON o.object_id = c.id
+WHERE o.type in ('P', 'FN', 'TF', 'TR', 'V', 'U')
+	  AND lower(c.text) LIKE '%rem%'
+ORDER BY o.name;
