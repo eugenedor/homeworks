ba7420fc3a1319613306c896914314b704db7589
Eugene Dorokhin <eugenedor1986@gmail.com>
2018-12-02 23:51:54 +0300

    DepthFirstSearch DFS 2.0

--- MsnrAndCnbl/MsnrAndCnbl/DepthFirstSearch.cs
+++ MsnrAndCnbl/MsnrAndCnbl/DepthFirstSearch.cs
@@ -10,43 +10,41 @@ namespace MsnrAndCnbl
     {
         public static bool DFS(Situation situation, List<Situation> listInSituation, out List<Situation> listOutSituation)
         {
-            listInSituation.Add(situation);                           // Список пройденных ситуаций.
-            listOutSituation = new List<Situation>(listInSituation);  // Возврат списка пройденных ситуаций. 
-            var term = false;                                         // Признак того, что достигнуто конечное состояние.
+            listInSituation.Add(situation);                                 // Список пройденных ситуаций.
+            listOutSituation = new List<Situation>(listInSituation);        // Возврат списка пройденных ситуаций.             
+
             if (situation.isEnd)
+                return true;
+
+            if (situation.IsDeadLock)
+                return false;
+
+            var term = false;                                               // Признак того, что достигнуто конечное состояние.
+            List<Action> listaction = Action.GenerationAction(situation);   // Генерация действий для ситуации (узла в дереве).
+            var situations = new List<Situation>();                         // Список дочерних ситуаций.
+            foreach (var act in listaction)
             {
-                term = true;
+                var newSituation = situation * act;
+
+                int numb = (from s in listInSituation
+                            where s.Msnr == newSituation.Msnr
+                                    && s.Cnbl == newSituation.Cnbl
+                                    && s.RvrBnk == newSituation.RvrBnk
+                            select s).Count<Situation>(); // Кол-во предыдущих ситуаций совпадающих с текущей дочерней.
+
+                if (numb > 0)           // Если кол-во больше 0, значит такая ситуация уже была, текущая дочерняя является тупиковой.
+                    newSituation.IsDeadLock = true;
+
+                situations.Add(newSituation);
             }
-            else
+
+            // Рекурсивный спуск будет продолжаться до обнаружения целевой вершины, 
+            // либо будет происходить перебор всех вершин, до последнего тупика.
+            foreach (var st in situations)
             {
-                if (!situation.IsDeadLock)
-                {
-                    List<Action> listaction = Action.GenerationAction(situation);   // Генерация действий для ситуации (узла в дереве).
-                    var situations = new List<Situation>();                         // Список дочерних ситуаций.
-                    foreach (var act in listaction)
-                    {
-                        var newSituation = situation * act;
-                        int numb = (from s in listInSituation
-                                    where s.Msnr == newSituation.Msnr 
-                                          && s.Cnbl == newSituation.Cnbl 
-                                          && s.RvrBnk == newSituation.RvrBnk
-                                    select s).Count<Situation>(); // Кол-во предыдущих ситуаций совпадающих с текущей дочерней.
-
-                        if (numb > 0)           // Если кол-во больше 0, значит такая ситуация уже была, текущая дочерняя является тупиковой.
-                            newSituation.IsDeadLock = true;
-
-                        situations.Add(newSituation);
-                    }
-
-                    // Рекурсивный спуск будет продолжаться до обнаружения целевой вершины, 
-                    // либо будет происходить перебор всех вершин, до последнего тупика.
-                    foreach (var st in situations)
-                    {
-                        term = DFS(st, listInSituation, out listOutSituation); // Спуск
-                        if (term)
-                            break;
-                    }
-                }
+                term = DFS(st, listInSituation, out listOutSituation); // Спуск
+                if (term)
+                    break;
             }
             return term;
         }
