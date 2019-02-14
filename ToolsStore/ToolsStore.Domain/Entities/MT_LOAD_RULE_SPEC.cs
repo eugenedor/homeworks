f58a8b6ebdccc81a71422b0698fd8cd1bd355530
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-02-14 22:59:10 +0300

    loadRuleSpec mc

--- ToolsStore/ToolsStore.Domain/Entities/MT_LOAD_RULE_SPEC.cs
+++ ToolsStore/ToolsStore.Domain/Entities/MT_LOAD_RULE_SPEC.cs
@@ -14,24 +14,21 @@ namespace ToolsStore.Domain.Entities
 
         public long LoadRuleId { get; set; }
 
-        [Required]
         public byte[] Data { get; set; }
 
-        [Required]
         [StringLength(50)]
         public string MimeType { get; set; }
 
         [Display(Name = "Наименование xsd-файла")]
-        [Required(ErrorMessage = "Введите наименование xsd-файла")]
         [StringLength(100)]
         public string FileName { get; set; }
+
+        public int? Size { get; set; }
 
         [Display(Name = "Путь к xsd-файлу")]
         [Required(ErrorMessage = "Укажите путь к xsd-файлу")]
         [StringLength(250)]
-        public string PathToFile { get; set; }
-
-        public int? Size { get; set; }
+        public string PathToFile { get; set; }        
 
         public bool IsActive { get; set; }
 
