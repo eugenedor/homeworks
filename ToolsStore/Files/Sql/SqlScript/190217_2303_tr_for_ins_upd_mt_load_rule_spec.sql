41405b202ae4d1f84e1a6cee33face101d781f4c
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-02-18 22:52:59 +0300

    test_tr_for_ins_upd_mt_load_rule_spec

--- ToolsStore/Files/Sql/SqlScript/190217_2303_tr_for_ins_upd_mt_load_rule_spec.sql
+++ ToolsStore/Files/Sql/SqlScript/190217_2303_tr_for_ins_upd_mt_load_rule_spec.sql
@@ -6,7 +6,7 @@ AS
 BEGIN TRAN
   DECLARE @error NVARCHAR(100);
 
-  --check--
+  --check filename--
   IF (EXISTS(SELECT i.LoadRuleSpecId
 			 FROM INSERTED i
 			 WHERE ISNULL(LTRIM(RTRIM(i.FileName)), '') = ''))
@@ -15,6 +15,16 @@ BEGIN TRAN
     RAISERROR(@error, 16, 1)
 	ROLLBACK TRAN    
   END
-  COMMIT TRAN
 
+  --check data--
+  IF (EXISTS(SELECT i.LoadRuleSpecId
+			 FROM INSERTED i
+			 WHERE NULLIF(i.Data, 0x00) IS NULL))
+  BEGIN
+	SET @error = 'Нет данных.';
+    RAISERROR(@error, 16, 1)
+	ROLLBACK TRAN    
+  END
+
+  COMMIT TRAN
 GO
