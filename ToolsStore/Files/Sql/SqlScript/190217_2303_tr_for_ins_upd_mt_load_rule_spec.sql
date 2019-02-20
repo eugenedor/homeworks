a2dabccee2d2e57c50c02662a27a916bffb268f7
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-02-21 00:03:03 +0300

    tr_for_ins_upd_mt_load_rule_spec mc

--- ToolsStore/Files/Sql/SqlScript/190217_2303_tr_for_ins_upd_mt_load_rule_spec.sql
+++ ToolsStore/Files/Sql/SqlScript/190217_2303_tr_for_ins_upd_mt_load_rule_spec.sql
@@ -36,5 +36,21 @@ BEGIN TRAN
 	ROLLBACK TRAN    
   END
 
+  --check ismain
+  IF (EXISTS(SELECT 1
+			 FROM dbo.MT_LOAD_RULE_SPEC lrs
+			      JOIN (SELECT ins.LoadRuleId
+						FROM INSERTED ins
+						GROUP BY ins.LoadRuleId) i
+				  ON lrs.LoadRuleId = i.LoadRuleId
+			 WHERE lrs.IsMain = 1
+			 GROUP BY lrs.LoadRuleId
+			 HAVING COUNT(lrs.LoadRuleSpecId) > 1))
+  BEGIN
+	SET @error = 'Главный в правиле загрузки должен быть один.';
+    RAISERROR(@error, 16, 1)
+	ROLLBACK TRAN    
+  END  
+  
   COMMIT TRAN
 GO
