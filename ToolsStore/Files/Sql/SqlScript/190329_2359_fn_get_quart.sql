89c7de798171a312d5ba5fe9cab1c309ac381f7c
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-03-30 00:19:00 +0300

    fn_get_quart mc

--- ToolsStore/Files/Sql/SqlScript/190329_2359_fn_get_quart.sql
+++ ToolsStore/Files/Sql/SqlScript/190329_2359_fn_get_quart.sql
@@ -1,26 +1,33 @@
-DECLARE @date DATETIME,
-        @dbeg DATETIME,
-		@qrt  SMALLINT
+CREATE FUNCTION dbo.FN_GET_QUART
+(
+    @date DATETIME,
+    @qrt  SMALLINT = 0
+)
+RETURNS @table TABLE (quart SMALLINT, dbeg DATETIME, dend DATETIME)
+AS
+BEGIN    
+	DECLARE @dbeg DATETIME;
+	SET @dbeg = CONVERT(DATETIME, CAST(YEAR(@date) AS NVARCHAR(4)) + '0101', 112);
 
-DECLARE @t TABLE(quart SMALLINT, dbeg DATETIME);
+	DECLARE @t TABLE(quart SMALLINT, dbeg DATETIME);
+	WITH t AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5)) v(n))
+	INSERT INTO @t (quart, dbeg)
+	SELECT t.n, DATEADD(qq, t.n - 1, @dbeg) AS dbeg
+	FROM t;
+	
+	INSERT INTO @table (quart, dbeg, dend)  
+	SELECT t.quart, t.dbeg, DATEADD(dd, -1, DATEADD(qq, 1, t.dbeg)) AS dend
+	FROM @t t
+		 JOIN (SELECT t.quart
+			   FROM @t t
+			   WHERE @date BETWEEN dbeg AND DATEADD(dd, -1, DATEADD(qq, 1, t.dbeg))) tcur
+	       ON t.quart BETWEEN tcur.quart-1 AND tcur.quart+1
+	WHERE CASE @qrt 
+			WHEN -1 THEN tcur.quart-1
+			WHEN 0 THEN tcur.quart
+			WHEN 1 THEN tcur.quart+1
+		  END = t.quart;
 
-SELECT @date = GETDATE(),       
-       @dbeg = CONVERT(DATETIME, CAST(YEAR(@date) AS NVARCHAR(4)) + '0101', 112),
-	   @qrt = 1;
-
-WITH t AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5)) v(n))
-INSERT INTO @t (quart,dbeg )
-SELECT t.n, DATEADD(qq, t.n-1, @dbeg) AS dbeg
-FROM t;
-
-SELECT t.quart, t.dbeg, DATEADD(dd, -1, DATEADD(qq, 1, t.dbeg)) AS dend
-FROM @t t
-     JOIN (SELECT t.quart
-		   FROM @t t
-           WHERE @date between dbeg and dateadd(dd, -1, dateadd(qq, 1, t.dbeg))) tcur
-on t.quart between tcur.quart-1 and tcur.quart+1
-where case @qrt 
-          when -1 then tcur.quart-1
-		  when 0 then tcur.quart
-		  when 1 then tcur.quart+1
-	  end = t.quart
+	RETURN;
+END
+GO
