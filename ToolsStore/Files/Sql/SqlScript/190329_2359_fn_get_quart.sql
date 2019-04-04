c8f138ce5c954d8081f27bd51d6d2eae3cdc38be
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-04-04 23:41:42 +0300

    fn_get_quart refactoring

--- ToolsStore/Files/Sql/SqlScript/190329_2359_fn_get_quart.sql
+++ ToolsStore/Files/Sql/SqlScript/190329_2359_fn_get_quart.sql
@@ -11,28 +11,27 @@ RETURNS @Quarter TABLE
 )
 AS
 BEGIN    
-	DECLARE @DateBegin DATETIME;
-	SET @DateBegin = CONVERT(DATETIME, CAST(YEAR(@Date) AS NVARCHAR(4)) + '0101', 112);
+	DECLARE @DateQ DATETIME;
+	SET @DateQ = CONVERT(DATETIME, CAST(YEAR(@Date) AS NVARCHAR(4)) + '0101', 112);
 
-	DECLARE @Tbl TABLE (Quart SMALLINT, DateBegin DATETIME);
-
-	WITH t AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5)) v(n))
-	INSERT INTO @Tbl (Quart, DateBegin)
-	SELECT t.n, DATEADD(qq, t.n - 1, @DateBegin) AS DateBegin
-	FROM t;
+	WITH t AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5)) v(n)),
+	     tbl AS (SELECT t.n AS Quart, DATEADD(qq, t.n - 1, @DateQ) AS DateBegin
+	             FROM t)
 	
 	INSERT INTO @Quarter (Quart, DateBegin, DateEnd)  
-	SELECT t.Quart, t.DateBegin, DATEADD(dd, -1, DATEADD(qq, 1, t.DateBegin)) AS DateEnd
-	FROM @Tbl t
-		 JOIN (SELECT t.Quart
-			   FROM @Tbl t
-			   WHERE @Date BETWEEN DateBegin AND DATEADD(dd, -1, DATEADD(qq, 1, t.DateBegin))) tcur
-	       ON t.Quart BETWEEN tcur.Quart-1 AND tcur.Quart+1
+	SELECT tq.Quart, 
+	       tq.DateBegin, 
+		   DATEADD(dd, -1, DATEADD(qq, 1, tq.DateBegin)) AS DateEnd
+	FROM tbl tq
+		 JOIN (SELECT Quart
+			   FROM tbl
+			   WHERE @Date BETWEEN DateBegin AND DATEADD(dd, -1, DATEADD(qq, 1, DateBegin))) tcur
+	       ON tq.Quart BETWEEN tcur.Quart-1 AND tcur.Quart+1
 	WHERE CASE @Quart 
 			WHEN -1 THEN tcur.Quart-1
 			WHEN 0 THEN tcur.Quart
 			WHEN 1 THEN tcur.Quart+1
-		  END = t.Quart;
+		  END = tq.Quart;
 
 	RETURN;
 END
