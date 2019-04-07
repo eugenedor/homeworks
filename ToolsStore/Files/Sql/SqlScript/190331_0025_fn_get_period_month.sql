4809e7e0f2d2cbeabd646ba90c8462a3b6649bbb
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-04-07 23:45:01 +0300

    fn_sql_period mc & test

--- ToolsStore/Files/Sql/SqlScript/190331_0025_fn_get_period_month.sql
+++ ToolsStore/Files/Sql/SqlScript/190331_0025_fn_get_period_month.sql
@@ -3,46 +3,43 @@ GO
 
 CREATE FUNCTION dbo.FN_GET_PERIOD_MONTH
 (
-    @dbeg DATETIME,
-    @dend DATETIME
+    @DateBegin DATETIME,
+    @DateEnd   DATETIME
 )
-RETURNS @table TABLE (year INT, month INT, dbeg DATETIME, dend DATETIME)
+RETURNS @PeriodsMonth TABLE 
+(
+	Year      INT, 
+	Month     INT, 
+	DateBegin DATETIME, 
+	DateEnd   DATETIME,
+	Quart     INT
+)
 AS
-BEGIN    
-	DECLARE @c        INT,
-			@yeardiff INT;
-	DECLARE @tyear  TABLE(y INT);
-	DECLARE @tmonth TABLE(m INT);
+BEGIN
+	WITH Periods AS 
+	(SELECT ROW_NUMBER() OVER (ORDER BY ty.Y, tm.M) AS Id,
+		    ty.Y, tm.M, 
+		    DATEADD(mm, tm.M -1, CONVERT(DATETIME, CAST(ty.Y AS NVARCHAR(4)) + '0101', 112)) AS DateBegin
+	 FROM (SELECT n1.Value AS Y
+	       FROM dbo.FN_GET_NUM(YEAR(@DateBegin), YEAR(@DateEnd)) n1) ty
+		   CROSS JOIN 
+		  (SELECT n2.Value AS M
+		   FROM dbo.FN_GET_NUM(1, 12) n2) tm)
 
-	SELECT @yeardiff = DATEDIFF(YY, @dbeg, @dend) + 1;
-
-	SET @c = 1;
-	WHILE (@c <= @yeardiff)
-	BEGIN
-	  INSERT INTO @tyear (y)
-	  SELECT YEAR(@dbeg) + @c - 1;
-	  SET @c = @c + 1;
-	END;
-
-	INSERT INTO @tmonth (m) VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12);
-
-	WITH tperiod AS 
-	(SELECT ROW_NUMBER() OVER (ORDER BY ty.y, tm.m) AS id,
-		   ty.y, tm.m , 
-		   DATEADD(mm, tm.m -1, CONVERT(DATETIME, CAST(ty.y AS NVARCHAR(4)) + '0101', 112)) AS dbeg
-	FROM @tyear ty
-		 CROSS JOIN @tmonth tm)
-
-    INSERT INTO @table (year, month, dbeg, dend)  
-	SELECT tp.y AS year, tp.m AS month, tp.dbeg, DATEADD(dd, -1, DATEADD(mm, 1, tp.dbeg)) AS dend
-	FROM tperiod tp
-	WHERE tp.id >= (SELECT DISTINCT tp1.id
-					FROM tperiod tp1
-					WHERE @dbeg BETWEEN tp1.dbeg AND DATEADD(dd, -1, DATEADD(mm, 1, tp1.dbeg)))
+    INSERT INTO @PeriodsMonth (Year, Month, DateBegin, DateEnd, Quart)  
+	SELECT tp.Y AS Year, 
+	       tp.M AS Month, 
+		   tp.DateBegin, 
+		   DATEADD(dd, -1, DATEADD(mm, 1, tp.DateBegin)) AS DateEnd,
+		   DATEPART(qq, tp.DateBegin) AS Quart
+	FROM Periods tp
+	WHERE tp.Id >= (SELECT DISTINCT tp1.Id
+					FROM Periods tp1
+					WHERE @DateBegin BETWEEN tp1.DateBegin AND DATEADD(dd, -1, DATEADD(mm, 1, tp1.DateBegin)))
 		  AND
-		  tp.id <= (SELECT DISTINCT tp2.id
-					FROM tperiod tp2
-					WHERE @dend BETWEEN tp2.dbeg AND DATEADD(dd, -1, DATEADD(mm, 1, tp2.dbeg)));
+		  tp.Id <= (SELECT DISTINCT tp2.Id
+					FROM Periods tp2
+					WHERE @DateEnd BETWEEN tp2.DateBegin AND DATEADD(dd, -1, DATEADD(mm, 1, tp2.DateBegin)));
 
 	RETURN;
 END
