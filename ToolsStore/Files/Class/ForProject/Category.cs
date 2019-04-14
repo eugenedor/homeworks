21c44e4ef9069f7c92c85f711c4eef70f54f7910
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-04-14 23:09:22 +0300

    brand class

---
+++ ToolsStore/Files/Class/ForProject/Category.cs
@@ -0,0 +1,157 @@
+﻿using System.Xml.Serialization;
+
+namespace ToolsStoreService.clss.category
+{
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class packet
+    {
+
+        private packetHdr hdrField;
+
+        private packetRec[] recField;
+
+        /// <remarks/>
+        public packetHdr hdr
+        {
+            get
+            {
+                return this.hdrField;
+            }
+            set
+            {
+                this.hdrField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("rec")]
+        public packetRec[] rec
+        {
+            get
+            {
+                return this.recField;
+            }
+            set
+            {
+                this.recField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class packetHdr
+    {
+
+        private string typeField;
+
+        private decimal versionField;
+
+        private string dateField;
+
+        /// <remarks/>
+        public string type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        public decimal version
+        {
+            get
+            {
+                return this.versionField;
+            }
+            set
+            {
+                this.versionField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string date
+        {
+            get
+            {
+                return this.dateField;
+            }
+            set
+            {
+                this.dateField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class packetRec
+    {
+
+        private string codeField;
+
+        private string nameField;
+
+        private int ordField;
+
+        /// <remarks/>
+        public string Code
+        {
+            get
+            {
+                return this.codeField;
+            }
+            set
+            {
+                this.codeField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public int Ord
+        {
+            get
+            {
+                return this.ordField;
+            }
+            set
+            {
+                this.ordField = value;
+            }
+        }
+    }
+}
