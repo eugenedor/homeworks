2f81d463ad6f95eb7e2b612a77540b33afaba4b0
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-07-28 23:54:16 +0300

    add FirstWCFService

---
+++ FirstWCFService/ConsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsApp.ServiceReference1;
+
+namespace ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cl = new FirstWCFServiceClient();
+
+            //First
+            string str = Console.ReadLine();
+            Console.WriteLine(cl.First(str));
+
+            //SecondAsync
+            Console.WriteLine(MethodAsync(cl).Result);
+
+            Console.ReadKey();
+        }
+
+        static async Task<string> MethodAsync(FirstWCFServiceClient c)
+        {
+            return await c.SecondAsync();
+        }
+    }
+}
---
+++ FirstWCFService/FirstWCFService/FirstWCFService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FirstWCFService
+{
+    public class FirstWCFService : IFirstWCFService
+    {
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string First(string name)
+        {
+            return "Welcome to the first method WCF web service application" + name;
+        }
+
+        public string Second()
+        {
+            return "Welcome to the second method WCF web service application!";
+        }
+    }
+}
