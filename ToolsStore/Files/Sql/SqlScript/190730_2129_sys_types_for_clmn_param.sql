40eb8088d7dc81c13ae4f4bf8069b3b45399e21b
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-07-30 21:30:49 +0300

    sys types for clmn and param

---
+++ ToolsStore/Files/Sql/SqlScript/190730_2129_sys_types_for_clmn_param.sql
@@ -0,0 +1,40 @@
+USE ToolsStore;
+GO
+
+--User table--
+SELECT o.name object_name, c.name column_name, c.max_length, c.precision, c.scale, c.is_nullable, t.name type_name
+FROM sys.objects AS o  
+     JOIN sys.columns AS c 
+	   ON o.object_id = c.object_id    
+     JOIN sys.types AS t 
+	   ON c.user_type_id=t.user_type_id  
+WHERE o.type = 'U'
+      and lower(o.name) = 'ct_category'; 
+
+
+--table function--
+SELECT o.name AS object_name, o.type_desc, p.parameter_id, p.name AS parameter_name, TYPE_NAME(p.user_type_id) AS parameter_type, p.max_length, p.precision, p.scale, p.is_output  
+FROM sys.objects o 
+     join sys.parameters p on o.object_id = p.object_id 
+	 join sys.types t on p.user_type_id = t.user_type_id
+WHERE o.type = 'TF' 
+      AND lower(o.name) = 'fn_get_numbers'   
+ORDER BY o.name;
+
+--scalar function--
+SELECT o.name AS object_name, o.type_desc, p.parameter_id, p.name AS parameter_name, t.name AS parameter_type, p.max_length, p.precision, p.scale, p.is_output  
+FROM sys.objects o 
+     join sys.parameters p on o.object_id = p.object_id 
+	 join sys.types t on p.user_type_id = t.user_type_id
+WHERE o.type = 'FN' 
+      AND lower(o.name) = 'fn_get_weekdays'   
+ORDER BY o.name;
+
+--stored procedure--
+SELECT o.name AS object_name, o.type_desc, p.parameter_id, p.name AS parameter_name, t.name AS parameter_type, p.max_length, p.precision, p.scale, p.is_output
+FROM sys.objects o 
+     join sys.parameters p on o.object_id = p.object_id 
+	 join sys.types t on p.user_type_id = t.user_type_id
+WHERE o.type = 'P' 
+      AND lower(o.name) = 'sp_im_category'   
+ORDER BY o.name;
