4a68df02d25a3e8ec3bd2e7acf05ac62afa2368e
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-09-08 23:50:10 +0300

    Nloguse

---
+++ Nloguse/Nloguse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Nloguse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = LogManager.GetCurrentClassLogger();
+
+            log.Trace("trace message");
+            log.Debug("debug message");
+            log.Info("info message");
+            log.Warn("warn message");
+            log.Error("error message");
+            log.Fatal("fatal message");
+        }
+    }
+}
