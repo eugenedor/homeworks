8385bfad276737357970eddec5dc6975f7d6eb4e
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-09-19 20:56:33 +0300

    documentation190919

--- ToolsStore/ToolsStoreService/file/FileManager.cs
+++ ToolsStore/ToolsStoreService/file/FileManager.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
 using System.IO;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Xml;
-using System.Xml.Schema;
 using System.Xml.Serialization;
 using ToolsStoreService.log;
 using ToolsStoreService.abstr;
