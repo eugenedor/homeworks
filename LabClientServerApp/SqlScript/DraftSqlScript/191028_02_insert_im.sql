c83425c0edc9c38375903733b517ff6190556a8c
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-10-28 21:10:50 +0300

    create buffer table im and insert

---
+++ LabClientServerApp/SqlScript/DraftSqlScript/191028_02_insert_im.sql
@@ -0,0 +1,74 @@
+--
+--INSERT IM_Animals
+--
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Amur_tiger', 'Амурский тигр', 'Хищные', 'Хордовые', 200);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Muskrat', 'Выхухоль', 'Насекомоядные', 'Хордовые', 0.45);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Comb_Newt', 'Гребенчатый тритон', 'Хвостатые земноводные', 'Хордовые', 0.013);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Steppe_eagle', 'Степной орел', 'Соколообразные', 'Хордовые', 3.5);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Japanese_crab', 'Японский краб', 'Десятиногие', 'Членистоногие', 15);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Azov_Beluga', 'Азовская белуга', 'Осетрообразные', 'Хордовые', 500);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Stag_beetle', 'Жук-олень', 'Жесткокрылые', 'Членистоногие', 0.08);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Bison', 'Зубр', 'Парнокопытные', 'Хордовые', 900);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Snow_Leopard', 'Снежный барс', 'Хищные', 'Хордовые', 55);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Legged_hatopterus', 'Разноногий хетоптерус', 'Щетинко-крылообразные', 'Кольчатые черви', 0.012);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Cat_snake', 'Кошачья змея', 'Чешуйчатые', 'NULL', 0.3);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Sharp-eyed_night', 'Остроухая ночница', 'Рукокрылые', 'Хордовые', 0.03);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Monk_seal', 'Тюлень-монах', 'Хищные', 'Хордовые', 300);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Passageway', 'Проехидна', 'Клоачные', 'Хордовые', 10);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Sumatran_Rhino', 'Носорог  суматранский', 'Непарнокопытные', 'Хордовые', 800);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Forest_giraffe', 'Лесной жираф', 'Парнокопытные', 'Хордовые', 230);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Marsupial_devil', 'Сумчатый дьявол', 'Хищные', 'Хордовые', 12);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Manul', 'Манул', 'Хищные', 'Хордовые', 4);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Stargazer', 'Звездонос', 'Насекомоядные', 'NULL', 0.085);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Grizzly_bear', 'Гризли медведь', 'Всеядные', 'Хордовые', 450);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Mountain_gorilla', 'Горная горилла', 'Приматы', 'Хордовые', 160);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Rhinoceros_Javanese', 'Носорог яванский', 'Непарнокопытные', 'Хордовые', 2100);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Big_panda', 'Большая панда', 'Хищные', 'Хордовые', 160);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Magellanic_Penguin', 'Магелланов пингвин', 'Пингвинообразные', 'Хордовые', 5);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Polar_bear', 'Белый медведь', 'Хищные', 'Хордовые', 350);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Pacific_walruses', 'Тихоокенские моржи', 'Хищные', 'Хордовые', 1000);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('California_condor', 'Калифорнийский кондор', 'Соколообразные', 'Хордовые', 14);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Pre-Wwalski_Horse', 'Лошадь Прежвальского', 'Непарнокопытные', 'Хордовые', 325);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Red-crowned_crane', 'Японский журавль', 'Журавлеобразные', 'NULL', 7.05);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Manatee', 'Ламантин', 'Сирены', 'Хордовые', 830);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Bactrian', 'Бактриан', 'Парнокопытные', 'Хордовые', 570);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Madagascar_shepherds', 'Мадагаскарские пастушки', 'Журавлеобразные', 'Хордовые', 2);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Grevys_Zebra', 'Зебра Греви', 'Непарнокопытные', 'Хордовые', 400);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Lemur', 'Лемур', 'Приматы', 'Хордовые', 3.05);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('a_lion', 'Лев', 'Хищные', 'Хордовые', 180);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Smoky_leopard', 'Дымчатый леопард', 'Хищные', 'Хордовые', 20);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Mouse', 'Мышь', 'Грызуны', 'Хордовые', 0.01);
+INSERT INTO dbo.IM_Animals (AnimalCode, AnimalName, SquadName, TypeName, TypicalWeight) VALUES ('Elephant_indian', 'Слон индийский', 'Хоботные', 'Хордовые', 3800);
+
+
+--
+--insert IM_Squads
+--
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Omnivores', 'Всеядные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Rodents', 'Грызуны');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Decapods', 'Десятиногие');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Winged', 'Жесткокрылые');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Crane-like', 'Журавлеобразные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Foul', 'Клоачные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Insectivores', 'Насекомоядные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Ungulate', 'Непарнокопытные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Sturgeon-like', 'Осетрообразные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Artiodactyls', 'Парнокопытные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Penguin-like', 'Пингвинообразные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Primates', 'Приматы');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Bats', 'Рукокрылые');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Sirens', 'Сирены');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Falconiformes', 'Соколообразные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Tailed amphibians', 'Хвостатые земноводные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Predatory', 'Хищные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Proboscis', 'Хоботные');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Scaly', 'Чешуйчатые');
+INSERT INTO dbo.IM_Squads (SquadCode, SquadName) VALUES ('Bristle-winged', 'Щетинко-крылообразные');
+
+
+--
+--insert IM_Types
+--
+INSERT INTO dbo.IM_Types (TypeCode, TypeName) VALUES ('Annelids', 'Кольчатые черви');
+INSERT INTO dbo.IM_Types (TypeCode, TypeName) VALUES ('Chordate', 'Хордовые');
+INSERT INTO dbo.IM_Types (TypeCode, TypeName) VALUES ('Arthropods', 'Членистоногие');
