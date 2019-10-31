79cec0d61fed73e4179918b021fd5a9d87842a55
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-10-31 20:20:16 +0300

    180329_list_tables mc

--- ToolsStore/Files/Sql/SqlScript/180329_list_tables.sql
+++ ToolsStore/Files/Sql/SqlScript/180329_list_tables.sql
@@ -2,13 +2,12 @@ USE ToolsStore;
 GO
 
 --1--
-SELECT t.* 
-FROM sys.tables t
-WHERE t.type_desc = 'USER_TABLE'
-ORDER BY t.name ASC;
+SELECT t.object_id, t.name object_name, t.*
+FROM sys.objects AS t  
+WHERE t.type = 'U'
+order by t.name ASC;
 
 --or--
-
 --2--
 SELECT t.* 
 FROM sysobjects t 
@@ -16,8 +15,14 @@ WHERE t.xtype = 'U'
 ORDER BY t.name ASC;
 
 --or--
-
 --3--
+SELECT t.* 
+FROM sys.tables t
+WHERE t.type_desc = 'USER_TABLE'
+ORDER BY t.name ASC;
+
+--or--
+--4--
 SELECT table_name 
 FROM information_schema.tables 
 ORDER BY table_name ASC;
