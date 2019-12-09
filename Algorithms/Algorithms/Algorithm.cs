acc4baa1555fd5af3881634e3f1d717dea43b7ed
Eugene Dorokhin <eugenedor1986@gmail.com>
2019-12-09 23:12:06 +0300

    BubbleSort

---
+++ Algorithms/Algorithms/Algorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Algorithm
+    {
+        /// <summary>
+        /// Сортировка пузырьком
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        int buf = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = buf;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cntItem = 10;
+
+            //BubbleSort
+            var array0 = SetArr(cntItem);
+            Console.WriteLine("Сортировка пузырьком:");
+            Console.WriteLine("До:");
+            PrintArr(array0);
+            Algorithm.BubbleSort(array0);
+            Console.WriteLine("После:");
+            PrintArr(array0);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        public static int[] SetArr(int c)
+        {
+            var arr = new int[c];
+            Random ran = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = ran.Next(1, 99);
+            }
+            return arr;
+        }
+
+        public static void PrintArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
