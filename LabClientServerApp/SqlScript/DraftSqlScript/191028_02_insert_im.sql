e17023fb2b676fca585036aeaf860a4c12a55f0a
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-01-19 00:47:39 +0300

    RedBook IM_Habitat mc

--- LabClientServerApp/SqlScript/DraftSqlScript/191028_02_insert_im.sql
+++ LabClientServerApp/SqlScript/DraftSqlScript/191028_02_insert_im.sql
@@ -1,3 +1,6 @@
+USE RedBook
+GO
+
 --
 --insert IM_Animals
 --
@@ -121,3 +124,74 @@ INSERT INTO dbo.IM_Continents (ContinentCode, ContinentName) VALUES ('Australia'
 INSERT INTO dbo.IM_Continents (ContinentCode, ContinentName) VALUES ('Eurasia', 'Евразия');
 INSERT INTO dbo.IM_Continents (ContinentCode, ContinentName) VALUES ('North America', 'Северная Америка');
 INSERT INTO dbo.IM_Continents (ContinentCode, ContinentName) VALUES ('South America', 'Южная Америка');
+
+--
+--insert IM_Habitat
+--
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Amur_tiger', 'RUS', 450);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Amur_tiger', 'CHN', 50);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Muskrat', 'RUS', 35000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Comb_Newt', 'RUS', 250);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Comb_Newt', 'LVA', 50);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Comb_Newt', 'BLR', 30);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Steppe_eagle', 'RUS', 20000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Steppe_eagle', 'KAZ', 10000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Japanese_crab', 'RUS', 100);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Japanese_crab', 'JPN', 500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Azov_Beluga', 'RUS', 24000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Stag_beetle', 'RUS', 20000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Bison', 'RUS', 500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Bison', 'UKR', 150);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Snow_Leopard', 'RUS', 200);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Snow_Leopard', 'CHN', 3500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Snow_Leopard', 'IND', 600);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Snow_Leopard', 'MNG', 1000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Snow_Leopard', 'TJK', 220);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Legged_hatopterus', 'RUS', 80);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Legged_hatopterus', 'CHN', 500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Cat_snake', 'RUS', 100);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Cat_snake', 'ARM', 600);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Sharp-eyed_night', 'RUS', 500000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Monk_seal', 'GRC', 1000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Passageway', 'IDN', 300000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Sumatran_Rhino', 'IDN', 30);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Sumatran_Rhino', 'MMR', 80);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Sumatran_Rhino', 'BGD', 120);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Forest_giraffe', 'COG', 10000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Marsupial_devil', 'AUS', 100000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Manul', 'RUS', 2300);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Manul', 'CHN', 1000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Stargazer', 'CAN', 40000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Grizzly_bear', 'USA', 600);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Mountain_gorilla', 'COG', 720);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Rhinoceros_Javanese', 'VNM', 60);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Big_panda', 'CHN', 1600);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Magellanic_Penguin', 'CHL', 800000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Magellanic_Penguin', 'ARG', 900000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Polar_bear', 'RUS', 18000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Polar_bear', 'CAN', 4000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Pacific_walruses', 'RUS', 200000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('California_condor', 'USA', 120);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Pre-Wwalski_Horse', 'CHN', 500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Pre-Wwalski_Horse', 'MNG', 400);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Red-crowned_crane', 'RUS', 500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Red-crowned_crane', 'JPN', 1500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Manatee', 'GAB', 1500);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Manatee', 'NGA', 1700);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Bactrian', 'KAZ', 200);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Bactrian', 'MNG', 700);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Madagascar_shepherds', 'MDG', 12000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Grevys_Zebra', 'KEN', 5000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Lemur', 'MDG', 1000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('a_lion', 'IND', 200);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('a_lion', 'BGD', 100);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('a_lion', 'KEN', 180);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Smoky_leopard', 'IDN', 570);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Smoky_leopard', 'VNM', 600);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Mouse', 'RUS', 40000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Mouse', 'CHN', 30000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Mouse', 'BLR', 42000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Mouse', 'UKR', 50000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Elephant_indian', 'IND', 10000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Elephant_indian', 'IDN', 9000);
+INSERT INTO dbo.IM_Habitat (AnimalCode, CountryCode, Population) VALUES ('Elephant_indian', 'VNM', 8000);
